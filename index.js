7e07b83fce6d36c3b5b0f8221cefe36c45013c87
ilhamfauzan630 <154285162+CH2-PS226@users.noreply.github.com>
2024-03-25 12:15:26 +0700

    create user routes

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-const bcrypt = require('bcrypt')
 const mongoose = require('mongoose')
 const session = require('express-session')
-const User = require('./auth/models/user')
+
+const port = 3000;
 
 mongoose.connect('mongodb://127.0.0.1/auth')
     .then((result) => {
@@ -25,66 +25,8 @@ app.use(session({
     saveUninitialized: false
 }))
 
-const auth = (req, res, next ) => {
-    if (!req.session.user_id) {
-        return res.redirect('/login')
-    }
-    next()
-}
+app.use('/', require('./routes/user'))
 
-const authenticated = (req, res, next ) => {
-    if (!req.session.user_id) {
-        next()
-    }
-    return res.redirect('/admin')
-}
-
-app.get('/', (req, res) => {
-    res.send('homepage')
+app.listen(port, () => {
+    console.log(`Server is running on port  http://localhost:${port}`)
 })
-
-app.get('/login', authenticated, (req, res) => {
-    res.render('login')
-})
-
-app.post('/login',async (req, res) => {
-    const { username, password } = req.body
-    const user = await User.findByCredentials( username, password )
-    if (user) {
-        req.session.user_id = user._id
-        res.redirect('/admin')
-    } else {
-        res.redirect('/login')
-    }
-})
-
-app.get('/register', (req, res) => {
-    res.render('register')
-})
-
-app.post('/register',async (req, res) => {
-    const { username, phonenumber, password } = req.body
-    const user = new User({ username, phonenumber, password })
-    await user.save()
-    req.session.user_id = user._id
-    res.redirect('/')
-})
-
-app.post('/logout',auth, (req, res) => {
-    req.session.destroy(() => {
-        res.redirect('/login')
-    })
-})
-
-app.get('/admin',auth , (req, res) => {
-    res.render('admin')
-})
-
-app.get('/profile/settings', auth, (req, res) => {
-    res.send('profile settings: ' + req.session.user_id)
-})
-
-app.listen(3000, () => {
-    console.log('Server is running on port  http://localhost:3000')
-})
-
