fff50d62b64663b2e3526d685cd913ff222a6e99
AzizAzmiJ-A-I <sukiastoria1945@gmail.com>
2024-06-04 20:05:04 +0700

    menambah post request di login

--- app/routes/login.route.js
+++ app/routes/login.route.js
@@ -3,6 +3,7 @@ module.exports = (app) => {
     const router = require('express').Router()
 
     router.get('/', users.loginUser)
+    router.post('/', users.loginUser)
 
     app.use('/api/login', router)
 }
