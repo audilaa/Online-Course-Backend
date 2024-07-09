96c169e7e1f61bc1f22331bb01693376d0ca8ce7
audilaa <154665339+audilaa@users.noreply.github.com>
2024-07-09 15:55:49 +0700

    test

--- app/routes/login.route.js
+++ app/routes/login.route.js
@@ -3,6 +3,7 @@ module.exports = (app) => {
     const router = require('express').Router()
 
     router.get('/', users.login)
+    router.post('/', users.login)
 
     app.use('/api/login', router)
 }
